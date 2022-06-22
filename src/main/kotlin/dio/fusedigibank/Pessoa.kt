67487843446cb9e14ee5d0125e0a8a0ee8d867f3
Henrique-Azevedo <henrique.azevedo.pro@gmail.com>
2022-06-22 14:47:02 -0300

    update to the private modifier to set function of cpf property

--- src/main/kotlin/dio/fusedigibank/Pessoa.kt
+++ src/main/kotlin/dio/fusedigibank/Pessoa.kt
@@ -1,18 +1,18 @@
 package dio.fusedigibank
 
 class Pessoa {
-
     var nome: String = "Henrique"
     var cpf: String = "123.123.123.11"
-
+    private set
 
 }
 
 fun main(){
-
     val henrique = Pessoa()
 
+    println(henrique)
     println(henrique.nome)
     println(henrique.cpf)
+
 }
 
